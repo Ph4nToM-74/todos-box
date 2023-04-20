67f48ebf06fbfcfd6194475ffa74724d7ee7f0d5
Subhatav Dhali <34691291+Ph4nToM-74@users.noreply.github.com>
2023-04-20 17:03:15 +0530

    Initiate Git repository with a basic Maven project

---
+++ todos-app/src/main/java/com/ph4ntom/of/codes/todosapp/TodosApplication.java
@@ -0,0 +1,13 @@
+package com.ph4ntom.of.codes.todosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosApplication {
+
+  public static void main(final String[] args) {
+
+    SpringApplication.run(TodosApplication.class, args);
+  }
+}
